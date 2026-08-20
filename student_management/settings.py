def4b7b6daba61974171a3ca8d1ce735e1ce6caa
Rahul-2577 <rp3618407@gmail.com>
2026-08-20 14:31:06 +0530

    Configure STATIC_ROOT for staticfiles collection

--- student_management/settings.py
+++ student_management/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Email
